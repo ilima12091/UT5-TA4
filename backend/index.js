4c41723b821ba36f0d09a061b83d3a4a828b26a6
ignacio.lima <ignacio.lima@inswitch.com>
2023-06-12 21:02:53 -0300

    Agrego la creación de una cookie

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 3000;
@@ -9,9 +12,34 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Working");
+const RSA_PRIVATE_KEY = fs.readFileSync("./private.key", "utf8");
+
+const validateEmailAndPassword = () => {
+  return true;
+};
+
+const findUserIdForEmail = (email) => {
+  return "123";
+};
+
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+  if (validateEmailAndPassword()) {
+    const userId = findUserIdForEmail(email);
+    const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
+      algorithm: "RS256",
+      expiresIn: 120,
+      subject: userId,
+    });
+    res.cookie("SESSIONID", jwtBearerToken, { httpOnly: false, secure: false });
+    res.send({
+      message: "Cookie Set",
+    });
+  } else {
+    res.status(401).send();
+  }
 });
 
 app.listen(port, () => {
